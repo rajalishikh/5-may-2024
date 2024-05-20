d13d5a5c76f5f6047bc0c6d083d022cf9bb59a86
raj <nahidujjamanraj@gmail.com>
2024-05-20 10:50:23 +0600

    In this section i add the data type

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,2 +1,8 @@
+# In this section I comment my name 
 print('my name is raj name too suna hi hoga ')
+# In this section I write my age 
 print(22)
+""" hi here is the most 
+comment in python 
+"""
+''''''
---
+++ modulePip.py
@@ -0,0 +1,5 @@
+name='my name is raj my old is 22'
+print(name)
+
+ablu='ablu is so mush first for programmin language he is start this 2022 '
+print(ablu)
