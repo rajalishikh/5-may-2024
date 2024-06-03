e72f0d23252def22fcc14f72ac3cdbbaedfd518c
raj <nahidujjamanraj@gmail.com>
2024-06-03 09:25:12 +0600

    In this section I run integer divison between integer value and floating value

---
+++ Expression2.py
@@ -0,0 +1,16 @@
+# string and numeric value can operate with * multiplication
+number1='raj'
+number2=3
+add=number1*number2
+
+print(add)
+# concatination two string 
+connumber1="raj"
+connumber2="bushra"
+print(connumber1+connumber2)
+
+# Integer division between float and integer value 
+integerdivisonnumber1=22
+integerdivisonnumber2=3
+integerdivison=integerdivisonnumber1//integerdivisonnumber2
+print(integerdivison)
