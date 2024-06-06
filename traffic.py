cb9d50bdec51c022a501d2af73805f4f83542854
raj <nahidujjamanraj@gmail.com>
2024-06-06 08:47:54 +0600

    in this part i lern condition part of traffic

---
+++ traffic.py
@@ -0,0 +1,9 @@
+signal=input('traffic color : ')
+if(signal == "red"):
+    print('stop')
+elif(signal == 'yellow'):
+    print('look')
+elif(signal == 'green'):
+    print('go')
+else:
+    print('Our traffic light is broken')
