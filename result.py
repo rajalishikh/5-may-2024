1749cbb1b75041dc8f64ec40d4a722c15c9b5956
raj <nahidujjamanraj@gmail.com>
2024-06-06 11:23:09 +0600

    In this section I know how can i am apply if elif else condition in python

---
+++ result.py
@@ -0,0 +1,9 @@
+result = int(input('please enter your exam result number  : '))
+if(result >= 90):
+    print(' A+')
+elif(result >= 80 and result < 90):
+    print(' B')
+elif(result >= 70 and result < 80):
+    print(' c')
+else:
+    print(' d')
