f79736a9a27b10d06d8fee4704eea79b003685b2
raj <nahidujjamanraj@gmail.com>
2024-06-07 10:12:31 +0600

    In this section I make proram for student who can read free in my class

--- food.py
+++ food.py
@@ -1,3 +1,6 @@
-food=input('give your food name ')
-eat='yes you can eat this food ' if food == 'cake' else 'dont eat'
-print(eat)
+# food=input('give your food name ')
+# eat='yes you can eat this food ' if food == 'cake' else 'dont eat'
+# print(eat)
+student=int(input('please tell us your father monthly income '))
+freePrivate='you can read private free ' if student<= 10000 else 'you can not  read free'
+print(freePrivate)
